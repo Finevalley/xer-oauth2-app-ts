9c626492f082ef9d1e452ac419361fc117670878
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2023-06-05 15:49:40 +0100

    Add webhooks authentication

--- src/functions/WebhooksHttpTrigger.ts
+++ src/functions/WebhooksHttpTrigger.ts
@@ -4,16 +4,17 @@ import {
 	HttpResponseInit,
 	InvocationContext,
 } from "@azure/functions";
+import * as crypto from "crypto";
 
 export async function WebhooksHttpTrigger(
 	request: HttpRequest,
 	context: InvocationContext
 ): Promise<HttpResponseInit> {
-	context.log(`Http function processed request for url "${request.url}"`);
+	context.log(`Webhook event recieved at "${request.url}"!`);
+	context.log(request.headers);
+	context.log(request.body);
 
-	const name = request.query.get("name") || (await request.text()) || "world";
-
-	return { body: `Hello, ${name}!` };
+	return { status: verifyWebhookEventSignature(request) ? 200 : 401 };
 }
 
 app.http("WebhooksHttpTrigger", {
@@ -22,3 +23,37 @@ app.http("WebhooksHttpTrigger", {
 	authLevel: "function",
 	handler: WebhooksHttpTrigger,
 });
+
+function verifyWebhookEventSignature(req: HttpRequest) {
+	const WEBHOOK_KEY = process.env.WEBHOOK_KEY;
+
+	if (!req.body) {
+		console.log("Signature failed. No request body was provided...");
+		return false;
+	}
+
+	if (!WEBHOOK_KEY) {
+		console.log("Signature failed. No WEBHOOK_KEY was provided...");
+		return false;
+	}
+
+	let computedSignature = crypto
+		.createHmac("sha256", WEBHOOK_KEY)
+		.update(req.body.toString())
+		.digest("base64");
+	let xeroSignature = req.headers.get("x-xero-signature");
+
+	if (xeroSignature === computedSignature) {
+		console.log("Signature passed! This is from Xero!");
+		return true;
+	} else {
+		// If this happens someone who is not Xero is sending you a webhook
+		console.log(
+			"Signature failed. Webhook might not be from Xero or you have misconfigured something..."
+		);
+		console.log(
+			`Got {${computedSignature}} when we were expecting {${xeroSignature}}`
+		);
+		return false;
+	}
+}
