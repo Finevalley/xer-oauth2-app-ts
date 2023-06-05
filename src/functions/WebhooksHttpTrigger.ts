afea91a493ccf2526f93fb245ba40bd82f661bec
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2023-06-05 16:11:30 +0100

    Fix logging for Azure

--- src/functions/WebhooksHttpTrigger.ts
+++ src/functions/WebhooksHttpTrigger.ts
@@ -12,9 +12,9 @@ export async function WebhooksHttpTrigger(
 ): Promise<HttpResponseInit> {
 	context.log(`Webhook event recieved at "${request.url}"!`);
 	context.log(request.headers);
-	context.log(request.body);
+	context.log(request.json);
 
-	return { status: verifyWebhookEventSignature(request) ? 200 : 401 };
+	return { status: verifyWebhookEventSignature(request, context) ? 200 : 401 };
 }
 
 app.http("WebhooksHttpTrigger", {
@@ -24,34 +24,37 @@ app.http("WebhooksHttpTrigger", {
 	handler: WebhooksHttpTrigger,
 });
 
-function verifyWebhookEventSignature(req: HttpRequest) {
+function verifyWebhookEventSignature(
+	request: HttpRequest,
+	context: InvocationContext
+) {
 	const WEBHOOK_KEY = process.env.WEBHOOK_KEY;
 
-	if (!req.body) {
-		console.log("Signature failed. No request body was provided...");
+	if (!request.body) {
+		context.log("Signature failed. No request body was provided...");
 		return false;
 	}
 
 	if (!WEBHOOK_KEY) {
-		console.log("Signature failed. No WEBHOOK_KEY was provided...");
+		context.log("Signature failed. No WEBHOOK_KEY was provided...");
 		return false;
 	}
 
 	let computedSignature = crypto
 		.createHmac("sha256", WEBHOOK_KEY)
-		.update(req.body.toString())
+		.update(request.body.toString())
 		.digest("base64");
-	let xeroSignature = req.headers.get("x-xero-signature");
+	let xeroSignature = request.headers.get("x-xero-signature");
 
 	if (xeroSignature === computedSignature) {
-		console.log("Signature passed! This is from Xero!");
+		context.log("Signature passed! This is from Xero!");
 		return true;
 	} else {
 		// If this happens someone who is not Xero is sending you a webhook
-		console.log(
+		context.log(
 			"Signature failed. Webhook might not be from Xero or you have misconfigured something..."
 		);
-		console.log(
+		context.log(
 			`Got {${computedSignature}} when we were expecting {${xeroSignature}}`
 		);
 		return false;
