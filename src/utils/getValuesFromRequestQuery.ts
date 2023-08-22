9da2cf4da63c1aa33efda4b41d99209334520e71
Elliott-Liu <16389800+Elliott-Liu@users.noreply.github.com>
2023-08-22 14:07:38 +0100

    Formatted

--- src/utils/getValuesFromRequestQuery.ts
+++ src/utils/getValuesFromRequestQuery.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 
 export function getValuesFromRequestQuery<T extends z.ZodTypeAny>(
 	request: HttpRequest,
-	querySchema: T
+	querySchema: T,
 ): z.SafeParseReturnType<z.ZodError, z.infer<typeof querySchema>> {
 	const values = valuesFromSearchParams(request.query);
 	return querySchema.safeParse(values);
@@ -13,9 +13,12 @@ export function getValuesFromRequestQuery<T extends z.ZodTypeAny>(
 }
 
 function valuesFromSearchParams(requestQuery: URLSearchParams) {
-	return Array.from(requestQuery.keys()).reduce((values, key) => {
-		const value = requestQuery.getAll(key);
-		values[key] = Array.isArray(value) ? value[0] : value;
-		return values;
-	}, {} as Record<string, string>);
+	return Array.from(requestQuery.keys()).reduce(
+		(values, key) => {
+			const value = requestQuery.getAll(key);
+			values[key] = Array.isArray(value) ? value[0] : value;
+			return values;
+		},
+		{} as Record<string, string>,
+	);
 }
